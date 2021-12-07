b4a824afe4ebd922d980e6190accf9992ad1456b
joe.wright <joe.wright@fanduel.com>
2021-12-07 07:21:51 +0000

    day 7, part 2

--- day_7.py
+++ day_7.py
@@ -10,6 +10,18 @@ def part_one(input):
     return min(distances.values())
 
 
+def triangular_number(n):
+    return int(n * (n + 1) / 2)
+
+
+def part_two(input):
+    distances = {}
+    for i in range(min(input), max(input)):
+        distances[i] = sum(map(lambda x: triangular_number(abs(x - i)), input))
+
+    return min(distances.values())
+
+
 advent_of_code(
     {
         "day": 7,
@@ -17,6 +29,17 @@ advent_of_code(
         "fn": part_one,
         "sample": input_7.sample(),
         "expected": 37,
+        "real": input_7.real(),
+    }
+)
+
+advent_of_code(
+    {
+        "day": 7,
+        "part": 2,
+        "fn": part_two,
+        "sample": input_7.sample(),
+        "expected": 168,
         "real": input_7.real(),
     }
 )
