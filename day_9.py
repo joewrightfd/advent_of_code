91b3b61ddf6e575495b2aa1187d3d64f6370e99f
joe.wright <joe.wright@fanduel.com>
2021-12-09 09:15:23 +0000

    day 9, part 2

--- day_9.py
+++ day_9.py
@@ -1,8 +1,23 @@
 import input_9
 from aoc import advent_of_code
+import math
 
 
-def part_one(input):
+def neighbouring_cells(x, y, x_height, y_height):
+    neighbours = []
+    if y > 0 and y < y_height:
+        neighbours.append([x, y - 1])
+    if y < (y_height - 1):
+        neighbours.append([x, y + 1])
+    if x > 0 and x < x_height:
+        neighbours.append([x - 1, y])
+    if x < (x_height - 1):
+        neighbours.append([x + 1, y])
+
+    return neighbours
+
+
+def find_lowest_points(input):
     x_height = len(input[0])
     y_height = len(input)
 
@@ -11,21 +26,40 @@ def part_one(input):
     for y in range(y_height):
         for x in range(x_height):
             cell = input[y][x]
-            neighbours = []
 
-            if y > 0 and y < y_height:
-                neighbours.append(input[y - 1][x])
-            if y < (y_height - 1):
-                neighbours.append(input[y + 1][x])
-            if x > 0 and x < x_height:
-                neighbours.append(input[y][x - 1])
-            if x < (x_height - 1):
-                neighbours.append(input[y][x + 1])
+            a = neighbouring_cells(x, y, x_height, y_height)
+            neighbours = map(lambda c: input[c[1]][c[0]], a)
 
             if cell < min(neighbours):
-                lowest_points.append(cell)
+                lowest_points.append([x, y])
 
-    return sum(map(lambda x: x + 1, lowest_points))
+    return lowest_points
+
+
+def part_one(input):
+    lowest_points = find_lowest_points(input)
+    return sum(map(lambda c: input[c[1]][c[0]] + 1, lowest_points))
+
+
+def part_two(input):
+    x_height = len(input[0])
+    y_height = len(input)
+    lowest_points = find_lowest_points(input)
+
+    basin_sizes = []
+    for lp in lowest_points:
+        x, y = lp
+        known = [lp]
+        cells_to_visit = neighbouring_cells(x, y, x_height, y_height)
+        for nx, ny in cells_to_visit:
+            if [nx, ny] not in known and input[ny][nx] != 9:
+                known.append([nx, ny])
+                cells_to_visit.extend(neighbouring_cells(nx, ny, x_height, y_height))
+
+        basin_sizes.append(len(known))
+
+    basin_sizes.sort(reverse=True)
+    return math.prod(basin_sizes[0:3])
 
 
 advent_of_code(
@@ -35,6 +69,17 @@ advent_of_code(
         "fn": part_one,
         "sample": input_9.sample(),
         "expected": 15,
+        "real": input_9.real(),
+    }
+)
+
+advent_of_code(
+    {
+        "day": 9,
+        "part": 2,
+        "fn": part_two,
+        "sample": input_9.sample(),
+        "expected": 1134,
         "real": input_9.real(),
     }
 )
