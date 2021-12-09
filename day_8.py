4291e2c950e8b822a0acd1509300b37366c94c5b
joe.wright <joe.wright@fanduel.com>
2021-12-09 21:07:33 +0000

    day 8, rename field

--- day_8.py
+++ day_8.py
@@ -27,62 +27,59 @@ def part_two(input):
         t = LettersAndPositions(symbols_by_length)
 
         # Perform sodoku given what we know
-        t.is_definitely("top", t.known_letters[7] - t.known_letters[1])
+        t.is_definitely("top", t.letters[7] - t.letters[1])
         t.is_the_most_common_within(
             "left_upper",
-            t.known_letters[4] - t.known_letters[1],
-            t.unknown_letters["0,6,9"],
+            t.letters[4] - t.letters[1],  # -> left_upper, mid
+            t.letters["0,6,9"],  # mid is not in 0
         )
         t.is_definitely(
             "mid",
-            t.known_letters[4] - t.known_letters[1] - set(t.at["left_upper"]),
+            t.letters[4] - t.letters[1] - set(t.at["left_upper"]),
         )
         t.is_the_most_common_within(
-            "right_lower", t.known_letters[1], t.unknown_letters["0,6,9"]
+            "right_lower",
+            t.letters[1],  # -> right_upper, right_lower
+            t.letters["0,6,9"],  # right_upper is not in 6
         )
         t.is_definitely(
             "right_upper",
-            t.known_letters[1] - set(t.at["right_lower"]),
+            t.letters[1] - set(t.at["right_lower"]),
         )
         t.is_the_most_common_within(
             "bottom",
-            t.known_letters[8] - t.known_letters[7] - t.known_letters[4],
-            t.unknown_letters["0,6,9"],
+            t.letters[8] - t.letters[7] - t.letters[4],  # -> left_lower, bottom
+            t.letters["0,6,9"],  # left_lower is not in 9
         )
         t.is_definitely(
             "left_lower",
-            t.known_letters[8]
-            - t.known_letters[7]
-            - t.known_letters[4]
-            - {t.at["bottom"]},
+            t.letters[8] - t.letters[7] - t.letters[4] - {t.at["bottom"]},
         )
 
         # Figure out the six unknown numbers now we have the pattern
-        t.known_letters[0] = next(
-            filter(lambda x: t.at["mid"] not in x, t.unknown_letters["0,6,9"])
+        t.letters[0] = next(filter(lambda x: t.at["mid"] not in x, t.letters["0,6,9"]))
+        t.letters[6] = next(
+            filter(lambda x: t.at["right_upper"] not in x, t.letters["0,6,9"])
         )
-        t.known_letters[6] = next(
-            filter(lambda x: t.at["right_upper"] not in x, t.unknown_letters["0,6,9"])
+        t.letters[9] = next(
+            filter(lambda x: t.at["left_lower"] not in x, t.letters["0,6,9"])
         )
-        t.known_letters[9] = next(
-            filter(lambda x: t.at["left_lower"] not in x, t.unknown_letters["0,6,9"])
-        )
-        t.known_letters[2] = next(
+        t.letters[2] = next(
             filter(
                 lambda x: t.at["right_upper"] in x and t.at["left_lower"] in x,
-                t.unknown_letters["2,3,5"],
+                t.letters["2,3,5"],
             )
         )
-        t.known_letters[3] = next(
+        t.letters[3] = next(
             filter(
                 lambda x: t.at["right_upper"] in x and t.at["right_lower"] in x,
-                t.unknown_letters["2,3,5"],
+                t.letters["2,3,5"],
             )
         )
-        t.known_letters[5] = next(
+        t.letters[5] = next(
             filter(
                 lambda x: t.at["left_upper"] in x and t.at["right_lower"] in x,
-                t.unknown_letters["2,3,5"],
+                t.letters["2,3,5"],
             )
         )
 
@@ -97,13 +94,11 @@ def part_two(input):
 
 class LettersAndPositions:
     def __init__(self, symbols_by_length):
-        self.known_letters = {
+        self.letters = {
             1: symbols_by_length[2][0],
             4: symbols_by_length[4][0],
             7: symbols_by_length[3][0],
             8: symbols_by_length[7][0],
-        }
-        self.unknown_letters = {
             "0,6,9": symbols_by_length[6],
             "2,3,5": symbols_by_length[5],
         }
@@ -117,11 +112,10 @@ class LettersAndPositions:
         always_present = Counter(
             filter(lambda x: x in possible, flatten(letters))
         ).most_common(1)
-
         self.is_definitely(position, always_present[0])
 
     def output_to_digit(self, value):
-        for number, letters in self.known_letters.items():
+        for number, letters in self.letters.items():
             if set(value) == letters:
                 return f"{number}"
 
