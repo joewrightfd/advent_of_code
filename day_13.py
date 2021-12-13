ab9951e67675097e3a952f0dff6c595d49c7d73d
joe.wright <joe.wright@fanduel.com>
2021-12-13 08:08:36 +0000

    day 13, part 2

--- day_13.py
+++ day_13.py
@@ -2,15 +2,31 @@ import input_13
 from aoc import advent_of_code
 
 
+def print_board(board):
+    for y in range(6):
+        print()
+        for x in range(39):
+            if (x, y) in board:
+                print("#", end=" ")
+            else:
+                print(".", end=" ")
+
+
 def part_one(input):
     folds, dots = input
-    fold_direction = folds[0]["direction"]
-    fold_amount = folds[0]["amount"]
 
     board = set()
     for x, y in dots:
         board.add((x, y))
 
+    fold_direction = folds[0]["direction"]
+    fold_amount = folds[0]["amount"]
+    new_board = perform_fold(board, fold_direction, fold_amount)
+
+    return len(new_board)
+
+
+def perform_fold(board, fold_direction, fold_amount):
     new_board = set()
     for x, y in board:
         if y > fold_amount and fold_direction == "y":
@@ -22,7 +38,24 @@ def part_one(input):
         else:
             new_board.add((x, y))
 
-    return len(new_board)
+    return new_board
+
+
+def part_two(input):
+    folds, dots = input
+
+    board = set()
+    for x, y in dots:
+        board.add((x, y))
+
+    for fold in folds:
+        fold_direction = fold["direction"]
+        fold_amount = fold["amount"]
+        board = perform_fold(board, fold_direction, fold_amount)
+
+    print_board(board)
+
+    return len(board)
 
 
 advent_of_code(
@@ -32,6 +65,17 @@ advent_of_code(
         "fn": part_one,
         "sample": input_13.sample(),
         "expected": 17,
+        "real": input_13.real(),
+    }
+)
+
+advent_of_code(
+    {
+        "day": 13,
+        "part": 2,
+        "fn": part_two,
+        "sample": input_13.sample(),
+        "expected": 16,
         "real": input_13.real(),
     }
 )
